c3e290cea7a6d81462c15737591a62b6817e4ddc
Nikhil Kapila <12816113+nkapila6@users.noreply.github.com>
2026-06-04 18:38:45 +0400

    added upto ch2l3 routing

--- main.go
+++ main.go
@@ -1,23 +1,51 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
-type health struct{}
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
-func (health) ServeHTTP(http.ResponseWriter, *http.Request) {}
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) reset() {
+	cfg.fileserverHits.Store(0)
+}
 
 func main() {
 	mux := http.NewServeMux()
 
-	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("./app/"))))
+	var apiCfg apiConfig
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(
+		http.StripPrefix("/app/", http.FileServer(http.Dir("./app/")))))
 
-	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("GET /healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
+	})
+
+	mux.HandleFunc("GET /metrics", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(fmt.Sprintf("Hits: %d", apiCfg.fileserverHits.Load())))
+	})
+
+	mux.HandleFunc("POST /reset", func(w http.ResponseWriter, r *http.Request) {
+		apiCfg.reset()
+		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(fmt.Sprintf("Resetted: %d", apiCfg.fileserverHits.Load())))
 	})
 
 	server := http.Server{
